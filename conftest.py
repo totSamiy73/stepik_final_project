8b88005fcb17d08072c91373dd5f74b9f4a9d74c
totSamiy73 <dmitriy73a@mail.ru>
2026-06-15 18:20:31 +0300

    add gitignore, init, conftest, requirements, test lesson_4_1_step_6

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+import time
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
+    parser.addoption('--language', action='store', default='ru',
+                         help="Choose language: '--language=en' or '--language=ru'")
+
+@pytest.fixture()
+def driver(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    driver = None
+    if browser_name == "chrome":
+        options_chrome = OptionsChrome()
+        options_chrome.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        driver = webdriver.Chrome(options=options_chrome)
+    elif browser_name == "firefox":
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference("intl.accept_languages", user_language)
+        driver = webdriver.Firefox(options=options_firefox)
+    driver.maximize_window()
+    # driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
---
+++ test_main_page.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+def test_guest_can_go_to_login_page(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    driver.get(link)
+    login_link = driver.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
